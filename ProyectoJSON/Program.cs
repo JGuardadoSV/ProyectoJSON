100e16d88523fbb367e4983a6e7edb9baa9836f8
JGuardadoSV <47751227+JGuardadoSV@users.noreply.github.com>
2021-06-10 09:03:57 -0600

    Agregar archivos de proyecto.

---
+++ ProyectoJSON/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ProyectoJSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Aplicacion de consola .net FRAMEWORK
+            //instalar nuget Install-Package Newtonsoft.Json -Version 13.0.1
+
+            //URL
+            string url = "https://jsonplaceholder.typicode.com/todos/1";
+
+            //visitando la url y guardando el resultado
+            HttpWebRequest peticion = (HttpWebRequest)WebRequest.Create(url);
+            //Extrayendo el resultado de la peticion 
+            HttpWebResponse respuesta = (HttpWebResponse)peticion.GetResponse();
+
+            Stream stream = respuesta.GetResponseStream();
+            StreamReader streamlector = new StreamReader(stream, System.Text.Encoding.GetEncoding(8));
+
+        }
+    }
+}
